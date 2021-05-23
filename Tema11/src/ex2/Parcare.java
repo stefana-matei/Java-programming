410a1ba3250d73cb0d645ad9a69bcc5897c18a72
Stefana Matei <stefana.otilia.matei@gmail.com>
2021-05-23 14:47:27 +0300

    Edit printf

--- Tema11/src/ex2/Parcare.java
+++ Tema11/src/ex2/Parcare.java
@@ -30,7 +30,7 @@ public class Parcare {
 		if(this.numarLocuriOcupate > 0) {
 			
 			this.numarLocuriOcupate--;
-			System.out.printf("\nA iesit o masina. In parcare sunt " + numarLocuriOcupate + " masini\n");
+			System.out.printf("\n- A iesit o masina. In parcare sunt " + numarLocuriOcupate + " masini\n");
 		}
 		
 		notify();
@@ -55,7 +55,7 @@ public class Parcare {
 		if (this.numarLocuriOcupate < capacitateParcare) {
 
 			this.numarLocuriOcupate++;
-			System.out.printf("\nA intrat o masina pe intrarea " + intrare + ". In parcare sunt " + numarLocuriOcupate
+			System.out.printf("\n+ A intrat o masina pe intrarea " + intrare + ". In parcare sunt " + numarLocuriOcupate
 					+ " masini.\n");
 		}
 
