3f60c3cbab77fe45463b910e4996a85d95e20476
Stefana Matei <stefana.otilia.matei@gmail.com>
2021-05-23 16:47:35 +0300

    ADD depunere, extragere methodes

--- Tema11/src/ex1/ContBancar.java
+++ Tema11/src/ex1/ContBancar.java
@@ -3,7 +3,46 @@ package ex1;
 public class ContBancar {
 
 	
+	private int sumaCont;
 	
 	
+	public ContBancar(int sumaCont)
+	{
+		this.sumaCont = sumaCont;
+	}
 	
+	
+	public synchronized void extragere(int sumaExtrasa)
+	{
+		
+		while (sumaExtrasa > this.sumaCont) {
+			
+			try {
+				
+				wait();
+			} catch (InterruptedException e) {
+
+				e.printStackTrace();
+			}
+			
+		}
+		
+		
+		if (sumaExtrasa <= this.sumaCont) {
+			
+			this.sumaCont -= sumaExtrasa;
+			System.out.printf("- A fost extrasa suma de " + sumaExtrasa + " RON si in cont au ramas " + sumaCont +" RON");
+		}
+		notify();
+	}
+	
+	
+	public synchronized void depunere(int sumaDepusa)
+	{
+		
+		this.sumaCont += sumaDepusa;
+		System.out.printf("+ - A fost depusa suma de " + sumaDepusa + " RON, in cont sunt : " + sumaCont + " RON");
+		notify();
+	}
+
 }
