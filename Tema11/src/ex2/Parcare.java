743c5820f06f19a825ed5fcc4679756f9c814e49
Stefana Matei <stefana.otilia.matei@gmail.com>
2021-05-23 12:10:25 +0300

    create Parcare class

---
+++ Tema11/src/ex2/Parcare.java
@@ -0,0 +1,27 @@
+package ex2;
+
+public class Parcare {
+
+	private int numarTotalLocuri;
+	private int numarLocuriOcupate;
+	
+	
+	public Parcare(int numarTotalLocuri, int numarLocuriOcupate)
+	{
+		this.numarTotalLocuri = numarTotalLocuri;
+		this.numarLocuriOcupate = numarLocuriOcupate;
+	}
+	
+	
+	public synchronized void Iesire()
+	{
+		
+	}
+	
+	
+	public synchronized void Intrare(int intrare)
+	{
+		
+	}
+	
+}
